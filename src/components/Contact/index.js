2a9a450d163e3dbbca0bf4bfd235d6e61eec737f
Man E <mannyportillo77@gmail.com>
2021-10-07 19:30:00 -0600

    new snapshot

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,72 +1,63 @@
-import React, {useState} from 'react';
-import {validateEmail} from '../../utils/helpers';
+import React, { useState } from 'react';
 
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
-    // add hook to manage form data & initialize the values of the state
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    const [errorMessage, setErrorMessage] = useState('');
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-    //destructure formState object into its named properties, use these constantst to assign the initial state
-    const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
 
-    //function to handle submission
-    function handleSubmit(e) {
-        e.preventDefault();
-        if (!errorMessage) {
-            setFormState({...formState, [e.target.name]: e.target.value })
-        }
-    };
-    
-    //function used to sync state of components with user input, event listener that fires with every keystroke
-    function handleChange(e) {
-         //validate email
-         if (e.target.name === 'email') {
-            const isValid = validateEmail(e.target.value);
-    
-        // isValid conditional statement
-            if (!isValid) {
-                setErrorMessage('Your email is invalid.');
-            } else {
-                setErrorMessage('');
-            }  
-        } else {
-            if (!e.target.value.length) {
-            setErrorMessage(`${e.target.name} is required.`);
-        } else {
-          setErrorMessage('');
-        }
-        }
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      setFormState({ [e.target.name]: e.target.value });
+      console.log('Form', formState);
+    }
+  };
 
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+  };
 
-    return (
+  return (
     <section>
-      <h1>Contact me</h1>
+      <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
-            <label htmlFor="name">Name:</label>
-            {/* add default , sync state of component using onChange attribute and function handleChange defined above */}
-            <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
         <div>
-            <label htmlFor="email">Email address:</label>
-            <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
-            <label htmlFor="message">Message:</label>
-            <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
         </div>
         {errorMessage && (
-            <div>
+          <div>
             <p className="error-text">{errorMessage}</p>
-            </div>
+          </div>
         )}
-        <button type="submit">Submit</button>
-    
+        <button data-testid="button" type="submit">Submit</button>
       </form>
     </section>
-    );
+  );
 }
 
 export default ContactForm;
